1fc1a7e8dd551c84b4a08957d6fecdc385123b8d
stefanostatuti <stefano.statuti@hotmail.it>
2013-08-22 17:06:45 +0200

    aggiunte info config di Smarty

--- siviaggiare/includes/config.inc.php
+++ siviaggiare/includes/config.inc.php
@@ -5,7 +5,7 @@
  */
 
 global $config;
-//sono da modificare con i vostri percorsi!!! CIAO STUPIDILLU
+//sono da modificare con i vostri percorsi!!! CIAO STUPIDILLU ciao
 $config['smarty']['template_dir'] =
 'C:\\xampp\\htdocs\\siviaggiare\\siviaggiare\\templates\\main\\template\\';
 $config['smarty']['compile_dir'] =
