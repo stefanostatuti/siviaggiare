d8c4a3568df05b193e61759f14516dce5848fd3c
stefanostatuti <stefano.statuti@hotmail.it>
2013-09-10 16:49:44 +0200

    MacOSX config

--- siviaggiare/includes/config.inc.php
+++ siviaggiare/includes/config.inc.php
@@ -28,6 +28,18 @@ $config['smarty']['config_dir'] =
 $config['smarty']['cache_dir'] =
     '/opt/lampp/htdocs/siviaggiare_gitHub/siviaggiare/templates/main/cache/';
 
+
+/*                          //Mac
+$config['smarty']['template_dir'] =
+    '/Applications/XAMPP/xamppfiles/htdocs/siviaggiare1/templates/main/template/';
+$config['smarty']['compile_dir'] =
+    '/Applications/XAMPP/xamppfiles/htdocs/siviaggiare1/templates/main/templates_c/';
+$config['smarty']['config_dir'] =
+    '/Applications/XAMPP/xamppfiles/htdocs/siviaggiare1/templates/main/configs/';
+$config['smarty']['cache_dir'] =
+    '/Applications/XAMPP/xamppfiles/htdocs/siviaggiare1/templates/main/cache/';
+*/
+
 $config['debug']=true;
 $config['mysql']['user'] = 'root';
 $config['mysql']['password'] = 'pippo';
