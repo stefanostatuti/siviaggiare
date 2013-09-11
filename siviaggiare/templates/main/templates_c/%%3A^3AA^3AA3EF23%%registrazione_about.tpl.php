89dfd80ca8010a61b2cbcf5eda758b9b4f205f2d
stefanostatuti <stefano.statuti@hotmail.it>
2013-09-11 10:45:16 +0200

    aggiornato commento

--- siviaggiare/templates/main/templates_c/%%3A^3AA^3AA3EF23%%registrazione_about.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2013-08-29 16:48:32
-         compiled from registrazione_about.tpl */ ?>
-<div class="sidebar">
-    <h3>Perch&egrave; registrarti?</h3>
-    <h6>Potrai condividere i tuoi viaggi
-        e essere d'aiuto agli altri utenti della comunit&agrave;</h6>
-    <a href="?controller=registrazione&task=registra"><h4>Registrati</h4></a>
-</div>
--- siviaggiare/templates/main/templates_c/%%51^51E^51E064E5%%conferma_registrazione.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2013-08-30 15:57:07
-         compiled from registrazione_conferma.tpl */ ?>
-<div class="content">
-    <h1 class="noicon">Conferma Registrazione</h1>
-    <h2 class="noicon">L'account &egrave; stato correttamente registrato, a breve sar&agrave; inviata una email di conferma con il codice di attivazione necessario per attivare l'account</h2>
-</div>
